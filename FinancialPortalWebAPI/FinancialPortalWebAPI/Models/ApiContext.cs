25366084e31c18006ccdce46a106e1a8711adf18
Damien Ostler <damienostler1@outlook.com>
2020-02-07 19:49:40 -0500

    Moving From Bit Bucket

---
+++ FinancialPortalWebAPI/FinancialPortalWebAPI/Models/ApiContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic
+using System.Linq;
+using System.Web;
+
+namespace FinancialPortalWebAPI.Models
+{
+    public class ApiContext:DbContext
+    {
+        public ApiContext():base("APIConnection")
+        {
+        }
+        
+        public async Task<List<>
+    }
+}
